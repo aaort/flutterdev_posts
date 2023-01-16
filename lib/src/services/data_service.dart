c4714f34c0b3030992a87db3da3bb9a26af347be
aort <artashesano@icloud.com>
2023-01-17 00:41:46 +0400

    Remove DataService constructor and update provider

--- lib/src/services/data_service.dart
+++ lib/src/services/data_service.dart
@@ -2,20 +2,29 @@ import 'package:dio/dio.dart';
 import 'package:dio_http_cache/dio_http_cache.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutterdev_posts/src/models/post.dart';
+import 'package:flutterdev_posts/src/services/cache.dart';
+import 'package:flutterdev_posts/src/services/connectivity_provider.dart';
 
 const _url = 'https://reddit.com/r/flutterdev/new.json';
 
-final postsProvider =
-    FutureProvider<List<Post>?>((ref) => DataService().getPosts());
+final postsProvider = FutureProvider<List<Post>?>((ref) async {
+  final isConnectedAsyncValue = ref.watch(connectivityProvider);
+  final cache = ref.watch(cacheProvider);
+  final isConnected = isConnectedAsyncValue.value;
+  // If internet connection exists fetch the data
+  if (isConnected == null) return [];
+  if (!isConnected) return await cache.getPosts();
+  if (isConnected) {
+    final posts = await DataService().getPosts();
+    if (posts == null) return null;
+    // Update cached data
+    cache.setPosts(posts);
+    return posts;
+  }
+});
 
 class DataService {
   static final _dio = Dio();
-
-  DataService() {
-    _dio.interceptors.add(DioCacheManager(
-            CacheConfig(baseUrl: 'https://reddit.com/r/flutterdev/new.json'))
-        .interceptor);
-  }
 
   Future<List<Post>?> getPosts({int page = 25}) async {
     // _dio.options.headers['Authorization'] = basicAuth;
@@ -26,6 +35,7 @@ class DataService {
     for (Map element in posts) {
       result.add(Post.fromMap(element['data'] as Map<String, dynamic>));
     }
+
     return result;
   }
 }
