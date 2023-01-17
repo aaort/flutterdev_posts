3ccaa5d2a044466bc831615e0b3bf82e2f933a86
aort <artashesano@icloud.com>
2023-01-17 10:45:46 +0400

    Remove fields on Post

--- lib/src/models/post.dart
+++ lib/src/models/post.dart
@@ -2,8 +2,6 @@ class Post {
   final String title;
   final String author;
   final String? thumbnail;
-  final int? thumbnailHeight;
-  final int? thumbnailWidth;
   final String selftext;
   final String subreddit;
   final int ups;
@@ -12,8 +10,6 @@ class Post {
     required this.title,
     required this.author,
     this.thumbnail,
-    this.thumbnailHeight,
-    this.thumbnailWidth,
     required this.selftext,
     required this.subreddit,
     required this.ups,
@@ -24,8 +20,6 @@ class Post {
       title: postMap['title'],
       author: postMap['author'],
       thumbnail: postMap['thumbnail'],
-      thumbnailHeight: postMap['thumbnail_height'],
-      thumbnailWidth: postMap['thumbnail_width'],
       selftext: postMap['selftext'],
       subreddit: postMap['subreddit'],
       ups: postMap['ups'],
@@ -37,8 +31,6 @@ class Post {
       'title': title,
       'author': author,
       'thumbnail': thumbnail,
-      'thumbnailHeight': thumbnailHeight,
-      'thumbnailWidth': thumbnailWidth,
       'selftext': selftext,
       'subreddit': subreddit,
       'ups': ups,
