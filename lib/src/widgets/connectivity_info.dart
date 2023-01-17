48bcee065070e49621d44f0af778317dda665177
aort <artashesano@icloud.com>
2023-01-18 00:16:32 +0400

    Improve formatting on connectivity_info

--- lib/src/widgets/connectivity_info.dart
+++ lib/src/widgets/connectivity_info.dart
@@ -3,9 +3,12 @@ import 'package:flutter/material.dart';
 const _connectionLostMessage = 'Sorry, internet connection is lost';
 const _connectionExistMessage = 'You\'re connected to the internet';
 
-void showConnectivityInfo(
-    {required BuildContext context, required bool isConnected}) {
+void showConnectivityInfo({
+  required BuildContext context,
+  required bool isConnected,
+}) {
   final message =
       isConnected ? _connectionExistMessage : _connectionLostMessage;
+
   ScaffoldMessenger.of(context).showSnackBar(SnackBar(content: Text(message)));
 }
