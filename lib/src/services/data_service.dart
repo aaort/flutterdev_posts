770b3691993179dddaf1c1682b1b0cf020bf11b0
aort <artashesano@icloud.com>
2023-01-18 00:03:32 +0400

    Remove unused imports in data_service

--- lib/src/services/data_service.dart
+++ lib/src/services/data_service.dart
@@ -2,8 +2,6 @@ import 'dart:convert' show jsonDecode;
 
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutterdev_posts/src/models/post.dart';
-import 'package:flutterdev_posts/src/services/cache.dart';
-import 'package:flutterdev_posts/src/services/connectivity_provider.dart';
 import 'package:http/http.dart' as http;
 
 const _url = 'https://reddit.com/r/flutterdev/new.json';
