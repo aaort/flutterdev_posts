57adbded0ad68707e20a5a517c9d37e52b65fb51
aort <artashesano@icloud.com>
2023-01-17 23:40:13 +0400

    Convert function body to liner expression in main.dart

--- lib/main.dart
+++ lib/main.dart
@@ -2,9 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
 import 'package:flutterdev_posts/src/screens/home.dart';
 
-void main() async {
-  runApp(const ProviderScope(child: App()));
-}
+void main() async => runApp(const ProviderScope(child: App()));
 
 class App extends StatelessWidget {
   const App({super.key});
