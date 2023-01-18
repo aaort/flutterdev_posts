35e01f946b4ff2ef97fe9f26701a5dcf4cd14237
aort <artashesano@icloud.com>
2023-01-18 23:49:13 +0400

    Update ThemeSwitchButton to fit new api

--- lib/src/widgets/theme_swtich_button.dart
+++ lib/src/widgets/theme_swtich_button.dart
@@ -1,29 +1,30 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-
-final numberProvider = StateProvider((ref) => 1);
+import 'package:flutterdev_posts/src/providers/theme_mode_provider.dart';
 
 class ThemeSwitchButton extends ConsumerWidget {
   const ThemeSwitchButton({super.key});
 
   @override
   Widget build(BuildContext context, WidgetRef ref) {
-    final number = ref.watch(numberProvider);
-    return GestureDetector(
-      onTap: () {
-        ref
-            .read(numberProvider.notifier)
-            .update((state) => state == 1 ? state = 2 : 1);
-      },
-      child: AnimatedSwitcher(
-        duration: const Duration(milliseconds: 350),
-        transitionBuilder: (child, anim) => RotationTransition(
-          turns: child.key == const ValueKey('dark_key')
-              ? Tween<double>(begin: 1, end: 0.75).animate(anim)
-              : Tween<double>(begin: 0.75, end: 1).animate(anim),
-          child: ScaleTransition(scale: anim, child: child),
+    final themeMode = ref.watch(themeModeProvider);
+    return Padding(
+      padding: const EdgeInsets.symmetric(horizontal: 20.0),
+      child: GestureDetector(
+        onTap: () {
+          // TODO: can be transformed into one line
+          ref.read(themeModeProvider.notifier).toggle();
+        },
+        child: AnimatedSwitcher(
+          duration: const Duration(milliseconds: 350),
+          transitionBuilder: (child, anim) => RotationTransition(
+            turns: child.key == const ValueKey('dark_key')
+                ? Tween<double>(begin: 1, end: 0.75).animate(anim)
+                : Tween<double>(begin: 0.75, end: 1).animate(anim),
+            child: ScaleTransition(scale: anim, child: child),
+          ),
+          child: themeMode.value == ThemeMode.dark ? _lightIcon : _darkIcon,
         ),
-        child: number == 2 ? _darkIcon : _lightIcon,
       ),
     );
   }
