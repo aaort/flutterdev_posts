e7dc4a6636f7f2734e2d22aea22effffb3384f2b
aort <artashesano@icloud.com>
2023-01-19 00:05:43 +0400

    Access cache by provider in theme_mode_provider

--- lib/src/providers/theme_mode_provider.dart
+++ lib/src/providers/theme_mode_provider.dart
@@ -2,7 +2,7 @@ import 'dart:async';
 
 import 'package:flutter/material.dart' show ThemeMode;
 import 'package:flutter_riverpod/flutter_riverpod.dart';
-import 'package:flutterdev_posts/src/services/cache.dart';
+import 'package:flutterdev_posts/src/providers/cache_provider.dart';
 
 final themeModeProvider =
     AsyncNotifierProvider<ThemeModeNotifier, ThemeMode>(ThemeModeNotifier.new);
@@ -10,13 +10,13 @@ final themeModeProvider =
 class ThemeModeNotifier extends AsyncNotifier<ThemeMode> {
   @override
   FutureOr<ThemeMode> build() {
-    return Cache().getThemeMode();
+    return ref.watch(cacheProvider).getThemeMode();
   }
 
   void toggle() async {
     state = await AsyncValue.guard(() async {
       return state.value == ThemeMode.dark ? ThemeMode.light : ThemeMode.dark;
     });
-    state.whenData((mode) => Cache().setThemeMode(mode));
+    state.whenData((mode) => ref.watch(cacheProvider).setThemeMode(mode));
   }
 }
