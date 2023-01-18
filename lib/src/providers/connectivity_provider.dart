610f9a3f657adcdfe8a1493e9a447c5c5e5806bf
aort <artashesano@icloud.com>
2023-01-18 17:35:23 +0400

    Implement isConnectedProvider

--- lib/src/providers/connectivity_provider.dart
+++ lib/src/providers/connectivity_provider.dart
@@ -1,8 +1,13 @@
 import 'package:connectivity_plus/connectivity_plus.dart';
-import 'package:flutter_riverpod/flutter_riverpod.dart' show StreamProvider;
+import 'package:flutter_riverpod/flutter_riverpod.dart'
+    show FutureProvider, StreamProvider;
 
 final connectivityProvider = StreamProvider<bool>((ref) {
   return Connectivity().onConnectivityChanged.map((event) {
     return !(event == ConnectivityResult.none);
   });
+});
+
+final isConnectedProvider = FutureProvider.autoDispose((ref) async {
+  return (await Connectivity().checkConnectivity()) != ConnectivityResult.none;
 });
