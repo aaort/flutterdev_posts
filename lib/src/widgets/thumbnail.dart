ad77fceb6a1fb83ccf5285292ba08227251f80dd
aort <artashesano@icloud.com>
2023-01-19 15:38:31 +0400

    Use Loading in Thumbnail and fix ugly progress indicator issue

--- lib/src/widgets/thumbnail.dart
+++ lib/src/widgets/thumbnail.dart
@@ -1,5 +1,6 @@
 import 'package:cached_network_image/cached_network_image.dart';
 import 'package:flutter/material.dart';
+import 'package:flutterdev_posts/src/widgets/loading.dart';
 
 class Thumbnail extends StatelessWidget {
   final String imageUrl;
@@ -9,14 +10,13 @@ class Thumbnail extends StatelessWidget {
   Widget build(BuildContext context) {
     return ClipRRect(
       borderRadius: BorderRadius.circular(3),
-      child: ConstrainedBox(
-        constraints: const BoxConstraints(minWidth: 90, minHeight: 100),
-        child: CachedNetworkImage(
-          imageUrl: imageUrl,
-          placeholder: (context, url) => const CircularProgressIndicator(),
-          errorWidget: (context, url, error) => const Icon(Icons.error),
-          fit: BoxFit.fill,
-        ),
+      child: CachedNetworkImage(
+        imageUrl: imageUrl,
+        placeholder: (context, url) => const Loading(),
+        errorWidget: (context, url, error) => const Icon(Icons.error),
+        height: 100,
+        width: 90,
+        fit: BoxFit.fill,
       ),
     );
   }
