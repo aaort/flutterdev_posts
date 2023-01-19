3420e418f8572011622f51a68bb1a9ac0af02c67
aort <artashesano@icloud.com>
2023-01-19 13:29:11 +0400

    Design related improvements on PostCard

--- lib/src/widgets/post_card.dart
+++ lib/src/widgets/post_card.dart
@@ -18,30 +18,33 @@ class PostCard extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     final thumbnail = post.thumbnail;
-    return InkWell(
-      onTap: () => onNavigateToDetails(context),
-      child: Card(
-        elevation: 5,
-        child: ConstrainedBox(
-          constraints: const BoxConstraints(minHeight: 80),
-          child: ListTile(
-            contentPadding: const EdgeInsets.symmetric(horizontal: 10),
-            title: Column(
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Text(
-                  'Posted by u/${post.author}',
-                  style: Theme.of(context).textTheme.bodySmall,
-                ),
-                const SizedBox(height: 10),
-                Text(post.title),
-              ],
-            ),
-            trailing: thumbnail != null && thumbnail.startsWith('http')
-                ? Thumbnail(imageUrl: thumbnail)
-                : null,
-          ),
+    return ConstrainedBox(
+      constraints: const BoxConstraints(minHeight: 80),
+      child: ListTile(
+        onTap: () => onNavigateToDetails(context),
+        contentPadding: const EdgeInsets.symmetric(
+          horizontal: 14,
+          vertical: 5,
         ),
+        shape: RoundedRectangleBorder(borderRadius: BorderRadius.circular(5)),
+        tileColor: Theme.of(context).cardColor.withOpacity(0.5),
+        title: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Text(
+              'Posted by u/${post.author}',
+              style: Theme.of(context).textTheme.bodySmall,
+            ),
+            const SizedBox(height: 10),
+            Text(
+              post.title,
+              style: Theme.of(context).textTheme.titleMedium,
+            ),
+          ],
+        ),
+        trailing: thumbnail != null && thumbnail.startsWith('http')
+            ? Thumbnail(imageUrl: thumbnail)
+            : null,
       ),
     );
   }
